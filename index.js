64a07e07f699710ff7448ec72aff733129580abe
Victor kichwen <victor.kichwen@student.moringaschool.com>
2024-09-24 21:19:30 +0300

    complete lab 2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+let currentUser = "Grass hopper"
+console.log(currentUser)
+ 
+let welcomeMessage = "Welcome to Flatbook, "+ currentUser + "!" ;
+console.log(welcomeMessage)
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, "+ currentUser + "!";
+console.log(excitedWelcomeMessage)
+
+let shortGreeting = "Welcome, " +  currentUser.charAt(0) + "!" 
+console.log(shortGreeting)
+
+
